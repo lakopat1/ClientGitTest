2428e78bdab3db3cbb3eecefe8132d227f41c101
artem <artemmarkvit@gmail.com>
2025-09-23 01:26:41 +0300

    перегрузка функции класса object

--- GitWorkApp/User.cs
+++ GitWorkApp/User.cs
@@ -15,5 +15,10 @@ namespace GitWorkApp
             Id = id;
             Name = name;
         }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
