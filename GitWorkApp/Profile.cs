cd1a7f9ed2fafda7c097a384cc8143086f2d8f19
artem <artemmarkvit@gmail.com>
2025-09-24 20:33:06 +0300

    Выполнил  task 1

---
+++ GitWorkApp/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Profile
+{
+    //Создать класс Профиль 
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Author { get; set; }
+
+
+    public Profile(string name, string description, string author)
+    {
+        Name = name;
+        Description = description;
+        Author = author;
+    }
+
+    //Выполнил таск 1
+}
